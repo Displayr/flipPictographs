175e505058b8a0b7ba96880c814a09e3af21d8d9
chschan <chschan@gmail.com>
2016-12-29 16:28:15 +1100

    Fix to previous commit

--- R/pictochart.R
+++ R/pictochart.R
@@ -459,7 +459,8 @@ PictoChart <- function(x,
         if (any(x >= total.icons))
             warning("Floating labels placed at invalid positions. Please increase total.icons\n")
 
-        label.float.position <- sprintf("%.0f:%.0f", floor(x/icon.ncol), x %% icon.ncol)
+        x.pos <- ceiling(x)
+        label.float.position <- sprintf("%d:%d", floor(x.pos/icon.ncol), x.pos %% icon.ncol)
         label.float.str <- sprintf("\"floatingLabels\":[{\"position\":\"%s\", \"text\":\"%s\",
                             \"font-size\":\"%fpx\",\"font-weight\":\"%s\",
                             \"font-family\":\"%s\", \"font-color\":\"%s\",
