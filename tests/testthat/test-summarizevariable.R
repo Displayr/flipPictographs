a114c29d1c7050617d9a26269edc8e8ca48b866e
chschan <chschan@gmail.com>
2018-11-29 16:37:20 +1100

    DS-2118: Add tests

---
+++ tests/testthat/test-summarizevariable.R
@@ -0,0 +1,23 @@
+context("SummarizeVariable")
+
+x0 <- 1:10
+x.logical <- c(TRUE, TRUE, FALSE, TRUE, TRUE, FALSE)
+x.numeric <- x0/10
+ww <- rep(c(1,5), each = 5)
+
+test_that("SummarizeVariable",
+{
+    expect_equal(SummarizeVariable(x0, "Sum"), 55)
+    expect_equal(SummarizeVariable(x0, "Sum", weight = ww), 215)
+    expect_equal(round(SummarizeVariable(x.logical, "Percentage"),2), 0.67)
+    expect_equal(round(SummarizeVariable(x.logical, "Average"),2), 0.67)
+    expect_equal(SummarizeVariable(x.logical, "Sum"), 4)
+    expect_warning(SummarizeVariable(x.logical, "Percentage", category="3"),
+                   "Showing percentage selected")
+
+    expect_equal(SummarizeVariable(x.numeric, "Average"), 0.55)
+    expect_warning(SummarizeVariable(x.numeric, "Percentage", category = 1),
+                   "A numeric variable was supplied")
+
+    expect_equal(SummarizeVariable(x.numeric, "Percentage", category = "0.4 - 0.6"), 0.3)
+})
