a3495f60259644dea41e3c4b40b7ca88fc4b5718
chschan <chschan@gmail.com>
2019-01-04 11:07:43 +1100

    DS-2285: Add option to find most frequent variable in SummarizeVariable

--- R/SummarizeVariable.R
+++ R/SummarizeVariable.R
@@ -1,13 +1,12 @@
 #' Summarizes a numeric or categorical variable for plotting
 #' @param x The input variable to summarize
-#' @param type The type of summary statistic to output. This should be one of 'Average', 'Sum' or 'Percentage'.
+#' @param type The type of summary statistic to output. This should be one of 'Average', 'Sum', 'Most frequent' or 'Percentage'.
 #' @param subset Option logical vector of the same length as \code{x} indicating whether or not to include
 #'   an observation in \code{x}.
 #' @param weights Sampling or replication weights. This is a numeric vector of the same length as \code{x}.
 #' @param category A comma-seperated list of the name or indices of the categories to include for 'Percentage'.
 #' @importFrom flipStatistics Mean Sum WeightedTable
-#' @importFrom flipTables SelectEntry
-#' @importFrom flipTransformations AsNumeric
+#' @importFrom flipTransformations AsNumeric TextAsVector
 #' @export
 SummarizeVariable <- function(x, type = c("Average", "Sum", "Percentage")[1], weights = NULL, subset = NULL, category = NULL)
 {
@@ -23,10 +22,19 @@ SummarizeVariable <- function(x, type = c("Average", "Sum", "Percentage")[1], we
             weights <- weights[subset]
     }
 
-    if (grepl("Average", type))
+    if (grepl("Average", type) || grepl("Mean", type))
         return(Mean(AsNumeric(x, binary = FALSE), weights = weights))
     if (grepl("Sum", type))
         return(Sum(AsNumeric(x, binary = FALSE), weights = weights))
+    if (grepl("Most frequent", type))
+    {
+        counts <- sort(WeightedTable(x, weights = weights), decreasing = TRUE)
+        tmp <- names(counts)[1]
+        if (is.numeric(x))
+            return(as.numeric(tmp))
+        else
+            return(x[x == tmp][1])
+    }
 
     if (is.null(weights))
         weights <- rep(1, length(x))
@@ -41,16 +49,16 @@ SummarizeVariable <- function(x, type = c("Average", "Sum", "Percentage")[1], we
 
     }
 
-	if (is.numeric(x))
+	if (is.numeric(x) && sum(nchar(category) > 0, na.rm = TRUE))
 	{
-		if (nchar(category) > 0 && grepl("\\d+\\s*-\\s*\\d+", category))
+		if (grepl("\\d+\\s*-\\s*\\d+", category))
 		{
 			cat.range <- as.numeric(trimws(strsplit(category, split = "-")[[1]]))
 			if (length(cat.range) == 2 && all(!is.na(cat.range)))
 			{
 				in.range <- x >= min(cat.range) & x <= max(cat.range)
 				return(sum(weights * in.range)/total)
-			} 
+			}
 		}
 		else if (is.numeric(x) && any(x != round(x, 0)))
 			warning("A numeric variable was supplied. Specify a range as the category (e.g. '1-5') or round variables to integers")
@@ -61,5 +69,5 @@ SummarizeVariable <- function(x, type = c("Average", "Sum", "Percentage")[1], we
     if (sum(nchar(category), na.rm = TRUE) == 0)
         stop("Select one or more categories from '", paste(names(counts), collapse="', '"), "'.")
 
-    return(sum(SelectEntry(counts, row=category, column = 1))/total)
+    return(sum(counts[TextAsVector(as.character(category))], na.rm = TRUE)/total)
 }
