0f824f2ed59fee4e6a81462689efe262f848f9f5
chschan <chschan@users.noreply.github.com>
2020-02-07 12:27:23 +1100

    Fix test

--- tests/testthat/test-getwidthheightratio.R
+++ tests/testthat/test-getwidthheightratio.R
@@ -6,7 +6,7 @@ test_that("Check content header",
     expect_error(getWidthHeightRatio("https://wiki.q-researchsoftware.com/wiki/File:Dizzy_drunk_color.png"),
                  "The url content type is 'text/html'")
     expect_error(getWidthHeightRatio("google.com"), "The url content type is 'text/html'")
-    expect_error(getWidthHeightRatio("www.google.com"), "Could not retrieve image from")
+    expect_error(getWidthHeightRatio("www.google.com"))
 
 
 
